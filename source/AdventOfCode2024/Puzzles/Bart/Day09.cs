acf5e53a3f5d94eb1c205317e7b1e5a6bcc84b6c
bart <bart.bruynooghe@gmail.com>
2024-12-12 22:31:50 +0100

    Day 9 part2

--- source/AdventOfCode2024/Puzzles/Bart/Day09.cs
+++ source/AdventOfCode2024/Puzzles/Bart/Day09.cs
@@ -41,13 +41,9 @@ public class Day09 : HappyPuzzleBase<ulong>
 				j++;
 			}
 		}
-		//PrintNmbrs(nmbrs);
 
 		//Sort
 		SortMemory(ref nmbrs);
-
-
-		//PrintNmbrs(nmbrs);
 
 		return CalculateChecksum(ref nmbrs);
 	}
@@ -103,8 +99,6 @@ public class Day09 : HappyPuzzleBase<ulong>
 			nmbrs[frontIndex] = nmbrs[backIndex];
 			nmbrs[backIndex] = -1;
 
-			//PrintNmbrs(nmbrs);
-
 			frontIndex++;
 			backIndex--;
 		}
@@ -112,74 +106,67 @@ public class Day09 : HappyPuzzleBase<ulong>
 
 	public override ulong SolvePart2(Input input)
 	{
-		throw new NotImplementedException();
-		var size = 0;
-		for (var i = 0; i < input.Lines[0].Length; i++)
-		{
-			size += (input.Lines[0][i] - '0');
-		}
+		var inputLength = input.Lines[0].Length;
 
-		scoped Span<int> nmbrs = stackalloc int[size];
+		scoped Span<(int memoryId, int start, int length)> files = stackalloc (int memoryId, int start,int length)[(inputLength+1)/2];
+		scoped Span<(int start, int length)> freeSpaces = stackalloc (int start, int length)[(inputLength+1)/2];
 
-		var index = 0;
-		var memoryId = 0;
-		var j = 0;
-		while(j < input.Lines[0].Length)
-		{
-			//mem block
-			var memSize = input.Lines[0][j] - '0';
-
-			for (var i = 0; i < memSize; i++)
-			{
-				nmbrs[index++] = memoryId;
-			}
-			memoryId++;
-			j++;
-
-			//free memory block
-			if (j < input.Lines[0].Length)
-			{
-				var freememSize = input.Lines[0][j] - '0';
-
-				for (var i = 0; i < freememSize; i++)
-				{
-					nmbrs[index++] = -1;
-				}
-				j++;
-			}
-		}
-		//PrintNmbrs(nmbrs);
-
-		//Sort
-		Part2_SortMemory(ref nmbrs);
-
-		//PrintNmbrs(nmbrs);
-
-		return CalculateChecksum(ref nmbrs);
+		ReadData(ref input, inputLength, ref files, ref freeSpaces);
+		SortFiles(ref files, ref freeSpaces);
+		return CalculateChecksum2(ref files);
 	}
 
-	private void Part2_SortMemory(ref Span<int> nmbrs)
+	private static void SortFiles(ref Span<(int memoryId, int start, int length)> files, ref Span<(int start, int length)> freeSpaces)
 	{
-		int backIndex = nmbrs.Length - 1;
-		while (backIndex > 0)
+		for (var i = files.Length-1; i >= 0; i--)
 		{
-			while (nmbrs[backIndex] == -1 && backIndex > 0)
+			for (var j = 0; j < freeSpaces.Length; j++)
 			{
-				backIndex--;
-			}
-			var endBlockIndex = backIndex;
-			while (nmbrs[backIndex] != -1 && backIndex > 0)
-			{
-				backIndex--;
-			}
-			var fromBlockIndex = backIndex;
-			var blocksize = endBlockIndex - fromBlockIndex;
+				if (freeSpaces[j].length >= files[i].length && freeSpaces[j].start < files[i].start)
+				{
+					files[i].start = freeSpaces[j].start;
 
-			var frontIndex = 0;
-			// while ()
-			// {
-			//
-			// }
+					freeSpaces[j].length -= files[i].length;
+					freeSpaces[j].start += files[i].length;
+					break;
+				}
+			}
 		}
+	}
+
+	private static void ReadData(ref Input input, int inputLength,ref Span<(int memoryId, int start, int length)> files, ref Span<(int start, int length)> freeSpaces)
+	{
+		var index = 0;
+		for (var i = 0; i < (inputLength+1)/2; i++)
+		{
+			var amount = input.Lines[0][2*i] - '0';
+			files[i] = (memoryId: i, start: index, amount);
+			index += amount;
+
+			if( 2*i+1 < inputLength)
+			{
+				var amountFreeSpaces = input.Lines[0][2*i + 1] - '0';
+				freeSpaces[i] = (start: index, length: amountFreeSpaces);
+				index += amountFreeSpaces;
+			}
+			else
+			{
+				freeSpaces[i] = (start: index, length: 0);
+			}
+		}
+	}
+
+	private static ulong CalculateChecksum2(ref Span<(int memoryId, int start, int length)> files)
+	{
+		ulong sum = 0;
+		for (var i = 0; i < files.Length; i++)
+		{
+			for (var j = 0; j < files[i].length; j++)
+			{
+				sum += (ulong) files[i].memoryId * (ulong) (files[i].start + j);
+			}
+		}
+
+		return sum;
 	}
 }
