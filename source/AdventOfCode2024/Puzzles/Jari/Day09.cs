26ce370a2b0bfa8dc4946dbb85a381694cd0ea52
jaduyve <jari.duyvejonck@outlook.com>
2024-12-15 22:55:10 +0100

    Solved part 2 day 9 - Jari

--- source/AdventOfCode2024/Puzzles/Jari/Day09.cs
+++ source/AdventOfCode2024/Puzzles/Jari/Day09.cs
@@ -118,12 +118,10 @@ public class Day09 : HappyPuzzleBase<long>
 		// calculate checksum
 		for (int i = 0; i < memory.Length; i++)
 		{
-			if (memory[i] == -1)
+			if (memory[i] != -1)
 			{
-				return checksum;
+				checksum += memory[i] * i;
 			}
-
-			checksum += memory[i] * i;
 		}
 
 		return checksum;
@@ -131,6 +129,108 @@ public class Day09 : HappyPuzzleBase<long>
 
 	public override long SolvePart2(Input input)
 	{
-		throw new NotImplementedException();
+		long checksum = 0;
+		int size = 0;
+		for (int i = 0; i < input.Text.Length; i++)
+		{
+			size += input.Text[i] - '0';
+		}
+
+		// expand compressed data
+		scoped Span<int> memory = stackalloc int[size];
+		int memoryPos = 0, counter = 0;
+		bool isFile = true;
+
+		for (int charIndex = 0; charIndex < input.Text.Length; charIndex++)
+		{
+			int value = isFile
+				? counter++
+				: -1;
+			for (int j = 0; j < input.Text[charIndex] - '0'; j++, memoryPos++)
+			{
+				memory[memoryPos] = value;
+			}
+
+			isFile = !isFile;
+		}
+
+		// optimize memory
+		for (int i = memory.Length - 1; i >= 0; i--)
+		{
+			// skip free space
+			while (i >= 0 && memory[i] == -1)
+			{
+				i--;
+			}
+
+			if (i < 0) break;
+
+			// find size of file id we want to optimize
+			var fileId = memory[i];
+			size = 0;
+			int j = i;
+			while (j >= 0 && memory[j] == fileId)
+			{
+				j--;
+				size++;
+			}
+
+			int startFreeSpaceIndex = FindStartFreeSpace(memory[..i], size);
+
+			if (startFreeSpaceIndex == -1)
+			{
+				i -= (size - 1);
+				continue;
+			}
+
+			for (int k = startFreeSpaceIndex; k < startFreeSpaceIndex + size; k++)
+			{
+				memory[k] = fileId;
+				memory[i--] = -1;
+			}
+
+			i++;
+		}
+
+		// calculate checksum
+		for (int i = 0; i < memory.Length; i++)
+		{
+			if (memory[i] != -1)
+			{
+				checksum += memory[i] * i;
+			}
+		}
+
+		return checksum;
+	}
+
+	private int FindStartFreeSpace(Span<int> memory, int neededSize)
+	{
+		int startFreeSpace = -1;
+		int foundSize = 0;
+		for (int i = 0; i < memory.Length; i++)
+		{
+			if (memory[i] == -1)
+			{
+				if (startFreeSpace == -1)
+				{
+					startFreeSpace = i;
+				}
+
+				foundSize++;
+			}
+			else
+			{
+				startFreeSpace = -1;
+				foundSize = 0;
+			}
+
+			if (foundSize == neededSize)
+			{
+				return startFreeSpace;
+			}
+		}
+
+		return -1;
 	}
 }
